e31b8d750c411e0ccd08528b25a12f23df154e4c
lightget2099 <aru54krat@gmail.com>
2026-06-23 18:30:39 +0300

    UserController + Registration dto added

---
+++ src/main/java/com/example/url_shortener/user/UserController.java
@@ -0,0 +1,19 @@
+package com.example.url_shortener.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public UserEntity register(@RequestBody UserRegistrationDto dto){
+       return userService.registerUser(dto.getUsername(), dto.getPassword());
+    }
+}
---
+++ src/main/java/com/example/url_shortener/user/UserRegistrationDto.java
@@ -0,0 +1,9 @@
+package com.example.url_shortener.user;
+
+import lombok.Value;
+
+@Value
+public class UserRegistrationDto {
+    String username;
+    String password;
+}
