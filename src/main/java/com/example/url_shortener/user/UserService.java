b655d4b5dc4fea7fb573257f5bcd4c7a0f48e89b
lightget2099 <aru54krat@gmail.com>
2026-06-23 18:15:59 +0300

    added UserService
    добавили перевірку унікальності, перевірку пароля на цифри та символи + зберігання пароля, але поки у відкритому вигляді, це допишу пізніше, коли буде security

---
+++ src/main/java/com/example/url_shortener/user/UserService.java
@@ -0,0 +1,30 @@
+package com.example.url_shortener.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserEntity registerUser(String username, String password) {
+        if(username.length() < 3 || username.length() > 20) {
+            throw new IllegalArgumentException("Username must be between 3 and 20 characters");
+        }
+
+        if(userRepository.existsByUsername(username)){
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        if(!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")){
+            throw new IllegalArgumentException("Password must contain at least + 1 uppercase letter, 1 digit, and 1 special character");
+        }
+
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        return userRepository.save(user);
+    }
+}
