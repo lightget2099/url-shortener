87294a2805f7e1eb5dee5807de899e81dc2c13fc
lightget2099 <aru54krat@gmail.com>
2026-06-23 15:39:59 +0300

    UserEntity + UserRepository added

---
+++ src/main/java/com/example/url_shortener/user/entities/UserEntity.java
@@ -0,0 +1,23 @@
+package com.example.url_shortener.user.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
