8e6256f0a01cc1dcb8f7935431a7177fc9d57f8a
lightget2099 <aru54krat@gmail.com>
2026-06-24 15:17:42 +0300

    UrlController added
    post + get ендпоінти.
    post - для генерації коротких кодів
    get - для редіректу на оригінальний url

---
+++ src/main/java/com/example/url_shortener/url/UrlController.java
@@ -0,0 +1,30 @@
+package com.example.url_shortener.url;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/urls")
+@RequiredArgsConstructor
+public class UrlController {
+    private final UrlService urlService;
+
+    @PostMapping
+    public ResponseEntity<UrlResponseDto> createShortenUrl(@RequestBody UrlRequestDto dto,
+                                                           @RequestParam Long userId) {
+        UrlResponseDto test = new UrlResponseDto(urlService.shortenUrl(dto.getUrl(), userId));
+        return ResponseEntity.ok(test);
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<?> getOriginalUrl(@PathVariable String code) {
+        String originalUrl = urlService.getOriginalUrl(code);
+        java.net.URI redirectUri = java.net.URI.create(originalUrl);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(redirectUri)
+                .build();
+    }
+}
