78ea0075022863211dee709ee13337170324bcff
lightget2099 <aru54krat@gmail.com>
2026-06-24 15:16:37 +0300

    added findByCode method

--- src/main/java/com/example/url_shortener/url/UrlRepository.java
+++ src/main/java/com/example/url_shortener/url/UrlRepository.java
@@ -2,5 +2,8 @@ package com.example.url_shortener.url;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+    Optional<UrlEntity> findByCode(String code);
 }
