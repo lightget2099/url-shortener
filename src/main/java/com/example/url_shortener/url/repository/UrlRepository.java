b87058332fec521086a07ef5a6eca3bdd758d9bb
lightget2099 <aru54krat@gmail.com>
2026-06-25 01:13:31 +0300

    updated UrlRepository
    додав метод findByUserId

--- src/main/java/com/example/url_shortener/url/repository/UrlRepository.java
+++ src/main/java/com/example/url_shortener/url/repository/UrlRepository.java
@@ -3,8 +3,10 @@ package com.example.url_shortener.url.repository;
 import com.example.url_shortener.url.entity.UrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
     Optional<UrlEntity> findByCode(String code);
+    List<UrlEntity> findByUserId(Long userId);
 }
