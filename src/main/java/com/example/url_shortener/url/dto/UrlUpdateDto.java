4fb5916e04984dfd117a3f63e30ac9ee63d15505
lightget2099 <aru54krat@gmail.com>
2026-07-02 02:06:38 +0300

    added swagger description for new DTO

--- src/main/java/com/example/url_shortener/url/dto/UrlUpdateDto.java
+++ src/main/java/com/example/url_shortener/url/dto/UrlUpdateDto.java
@@ -1,5 +1,6 @@
 package com.example.url_shortener.url.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Value;
 
@@ -7,5 +8,7 @@ import lombok.Value;
 @SuppressWarnings("ClassCanBeRecord")
 public class UrlUpdateDto {
     @NotBlank(message = "Новий URL не може бути порожнім")
+    @Schema(description = "Новий URL для заміни старого",
+            example = "https://github.com")
     String newUrl;
 }
